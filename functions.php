0b3ca8f6bd78268c826d727377f000ecc00fb5ce
Takehito Sumimura <tsumimura@my.bcit.ca>
2024-09-21 16:59:43 -0700

    changed style

--- functions.php
+++ functions.php
@@ -335,9 +335,10 @@ add_action( 'wp_enqueue_scripts', 'enqueue_toggle_dropdown' );
 
 
 function school_theme_enqueue_styles() {
-    // Enqueue Google Fonts
-    wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&display=swap', false );
+    // Enqueue Rubik Google Font
+    wp_enqueue_style( 'custom-google-fonts', 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,700;1,400&display=swap', false );
 }
 add_action( 'wp_enqueue_scripts', 'school_theme_enqueue_styles' );
+
 
 ?>
